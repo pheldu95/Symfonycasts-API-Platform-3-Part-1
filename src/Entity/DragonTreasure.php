47a3c2de12d56d0afabe28292d64cfe4b91e43f6
pheldu95 <phelps.duncan@gmail.com>
2023-07-20 13:55:30 -0500

    chapter 11. used serializer to make it so we set the treasure name in the constructor. this means it can only be set when it's first created

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -13,6 +13,7 @@ use Carbon\Carbon;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
 #[ApiResource(
@@ -64,8 +65,9 @@ class DragonTreasure
     #[ORM\Column]
     private ?bool $isPublished = false;
 
-    public function __construct()
+    public function __construct(string $name = null)
     {
+        $this->name = $name;
         $this->plunderedAt = new \DateTimeImmutable();
     }
 
@@ -77,13 +79,6 @@ class DragonTreasure
     public function getName(): ?string
     {
         return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
     }
 
     public function getDescription(): ?string
@@ -99,6 +94,8 @@ class DragonTreasure
     }
 
     #[Groups('treasure:write')]
+    //control the name of the attribute/ here we will make it 'description' instead of the automatic texDescription name it is given
+    #[SerializedName('description')]
     public function setTextDescription(string $description): static
     {
         $this->description = nl2br($description);
