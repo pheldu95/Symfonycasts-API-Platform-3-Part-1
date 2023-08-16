8934864147328b35169a065b7d6c60c21d85ad79
pheldu95 <phelps.duncan@gmail.com>
2023-08-16 13:16:44 -0500

    chapter 22. Made it so we can do an 'Embedded Write' for a treasure, meaning we can update a treasure owner's username while we are updating the treasure as well

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -99,6 +99,7 @@ class DragonTreasure
     #[ORM\ManyToOne(inversedBy: 'dragonTreasures')]
     #[ORM\JoinColumn(nullable: false)]
     #[Groups(['treasure:read', 'treasure:write'])]
+    #[Assert\Valid]
     private ?User $owner = null;
 
     public function __construct(string $name = null)
