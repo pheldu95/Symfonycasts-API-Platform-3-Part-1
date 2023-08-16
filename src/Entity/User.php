8243601be80ce019a1071c1284b9e1cff5f16b3f
pheldu95 <phelps.duncan@gmail.com>
2023-08-16 13:25:07 -0500

    chapter 23. made User  collection writable. So when we are making a new user, we can assign existing treasures to them

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -51,7 +51,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $username = null;
 
     #[ORM\OneToMany(mappedBy: 'owner', targetEntity: DragonTreasure::class)]
-    #[Groups(['user:read'])]
+    #[Groups(['user:read', 'user:write'])]
     private Collection $dragonTreasures;
 
     public function __construct()
